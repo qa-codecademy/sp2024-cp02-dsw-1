d8b02fec1c818990762e636824b45c2e5151c265
nlazov <nikolalazov.nl@gmail.com>
2024-07-08 16:04:51 +0200

    contact form updates

--- contact-page/contact.js
+++ contact-page/contact.js
@@ -18,7 +18,7 @@ inputs.forEach((input) => {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  const contactForm = document.getElementById("contactForm");
+  const contactForm = document.getElementById("contact-form");
 
   contactForm.addEventListener("submit", function (event) {
     event.preventDefault();
